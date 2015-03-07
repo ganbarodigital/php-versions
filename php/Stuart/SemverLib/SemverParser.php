051bbc262dcb2fa0bef216c7e9632abd5a0922a3
Stuart Herbert <stuart@stuartherbert.com>
2015-03-07 18:29:53 +0000

    Fix: we only need the one regex now

--- php/Stuart/SemverLib/SemverParser.php
+++ php/Stuart/SemverLib/SemverParser.php
@@ -64,25 +64,14 @@ class SemverParser
      */
     protected function parseVersionString($versionString)
     {
-        // one regex for each supported structure
+        // one regex to rule them all
         //
-        // I'm using individual regexes here because it keeps things very
-        // readable indeed
-        $regexes = [
-            "%^(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)(\.(?P<patchLevel>0|[1-9]\d*)){0,1}(-(?P<preReleaseVersion>(0|[1-9]\d*|\d*|[a-zA-Z-][0-9a-zA-Z-]*)(\.(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*)){0,1}(\+(?P<buildNumber>[0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*)){0,1}$%"
-            // "|(?P<Major>[0-9]+)\\.(?P<Minor>[0-9]+)\\.(?P<Patchlevel>[0-9]+)-(?P<Stability>[0-9A-Za-z.]+)\+(?P<Release>[0-9A-Za-z-.]+)|", // x.y.z-<stability>-r
-            // "|(?P<Major>[0-9]+)\\.(?P<Minor>[0-9]+)\\.(?P<Patchlevel>[0-9]+)|",                                          // x.y.z
-            // "|(?P<Major>[0-9]+)\\.(?P<Minor>[0-9]+)\\.(?P<Patchlevel>[0-9]+)-(?P<Release>[0-9]+)|",                      // x.y.z-r
-            // "|(?P<Major>[0-9]+)\\.(?P<Minor>[0-9]+)-(?P<Stability>[^-]+)-(?P<Release>[0-9]+)|",                          // x.y-<stability>-r
-            // "|(?P<Major>[0-9]+)\\.(?P<Minor>[0-9]+)$|",                                                                  // x.y
-            // "|(?P<Major>[0-9]+)\\.(?P<Minor>[0-9]+)-(?P<Release>[0-9]+)|",                                               // x.y-r
-        ];
+        // based on a regex proposed in the semver.org Github issues list
+        $regex = "%^(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)(\.(?P<patchLevel>0|[1-9]\d*)){0,1}(-(?P<preReleaseVersion>(0|[1-9]\d*|\d*|[a-zA-Z-][0-9a-zA-Z-]*)(\.(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*)){0,1}(\+(?P<buildNumber>[0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*)){0,1}$%";
 
-        foreach ($regexes as $regex) {
-            $matches = [];
-            if (preg_match($regex, $versionString, $matches)) {
-                return $matches;
-            }
+        $matches = [];
+        if (preg_match($regex, $versionString, $matches)) {
+            return $matches;
         }
 
         // if we get here, then nothing matched
