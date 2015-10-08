5422f8e21b0075ea43de5224a6876a3a31309c77
Stuart Herbert <stuart@stuartherbert.com>
2015-10-08 22:24:33 +0100

    New: VersionRange value object

---
+++ src/VersionRanges/Types/VersionRange.php
@@ -0,0 +1,102 @@
+<?php
+
+/**
+ * Copyright (c) 2015-present Ganbaro Digital Ltd.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the name of the copyright holders nor the names of the
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @category  Libraries
+ * @package   Versions/VersionRanges
+ * @author    Stuart Herbert <stuherbert@ganbarodigital.com>
+ * @copyright 2015-present Ganbaro Digital Ltd www.ganbarodigital.com
+ * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @link      http://code.ganbarodigital.com/php-versions
+ */
+
+namespace GanbaroDigital\Versions\VersionRanges\Types;
+
+use ArrayIterator;
+use GanbaroDigital\Reflection\Requirements\RequireObjectOfType;
+use GanbaroDigital\Reflection\Requirements\RequireTraversable;
+use GanbaroDigital\Versions\Exceptions\E4xx_UnsupportedType;
+use IteratorAggregate;
+use Traversable;
+
+/**
+ * Represents a parsed range of versions
+ */
+class VersionRange implements IteratorAggregate
+{
+    /**
+     * a list of one or more range expressions
+     *
+     * @var array
+     */
+    private $rangeExpressions = [];
+
+    /**
+     * our constructor
+     *
+     * @param array<ComparisonExpression> $rangeExpressions
+     *        the list of comparison expressions that make up this version range
+     */
+    public function __construct($rangeExpressions)
+    {
+        // robustness
+        RequireTraversable::check($rangeExpressions, E4xx_UnsupportedType::class);
+
+        $this->rangeExpressions = [];
+        foreach ($rangeExpressions as $rangeExpression) {
+            RequireObjectOfType::check($rangeExpression, ComparisonExpression::class, E4xx_UnsupportedType::class);
+            $this->rangeExpressions[] = $rangeExpression;
+        }
+    }
+
+    /**
+     * support foreach() loops over our data
+     *
+     * @return Traversable
+     */
+    public function getIterator()
+    {
+        return new ArrayIterator($this->rangeExpressions);
+    }
+
+    /**
+     * return our data as an array
+     *
+     * @return array<ComparisonExpression>
+     */
+    public function toArray()
+    {
+        return $this->rangeExpressions;
+    }
+}
